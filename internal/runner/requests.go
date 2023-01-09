af90a64fb07d6a1186602be719cac9b6138c6f03
canc3s <cances@foxmail.com>
2023-01-09 11:49:41 +0800

    fix: issues #8
    fix: issues #8

--- internal/runner/requests.go
+++ internal/runner/requests.go
@@ -6,10 +6,10 @@ import (
 	"github.com/antchfx/htmlquery"
 	"github.com/canc3s/cSubsidiary/internal/gologger"
 	"golang.org/x/net/html"
+	"io"
 	"io/ioutil"
 	"net"
 	"net/http"
-	"net/url"
 	"regexp"
 	"strconv"
 	"strings"
@@ -63,7 +63,7 @@ func DefaultTransport() *http.Transport {
 	return transport
 }
 
-func GetPage(url string, options *Options) Response {
+func GetPage(url string,method string, data io.Reader, options *Options) Response {
 
 	var transport = DefaultTransport()
 	var client = &http.Client{
@@ -73,8 +73,9 @@ func GetPage(url string, options *Options) Response {
 			return http.ErrUseLastResponse /* 不进入重定向 */
 		},
 	}
-	req, _ := http.NewRequest("GET", url, nil)
+	req, _ := http.NewRequest(method, url, data)
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0")
+	req.Header.Set("Content-Type", "application/json")
 	if options.Cookie != "" {
 		req.Header.Set("Cookie", options.Cookie)
 	}
@@ -100,33 +101,11 @@ func GetPage(url string, options *Options) Response {
 }
 
 func ListInvest(options *Options) []Subsidiary {
-	var transport = DefaultTransport()
-	var client = &http.Client{
-		Transport: transport,
-		//Timeout:       time.Duration(options.Timeout),
-		CheckRedirect: func(req *http.Request, via []*http.Request) error {
-			return http.ErrUseLastResponse /* 不进入重定向 */
-		},
-	}
-	body := strings.NewReader("{\"gid\":\""+options.CompanyID+"\",\"pageSize\":200,\"pageNum\":1}")
-	req, _ := http.NewRequest("POST", "https://capi.tianyancha.com/cloud-company-background/company/investListV2", body)
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0")
-	req.Header.Set("Content-Type", "application/json")
-	resp, err := client.Do(req)
-	if err != nil {
-		gologger.Fatalf("请求发生错误，请检查网络连接\n%s\n", err)
-	}
-
-	if resp.StatusCode == 403 {
-		gologger.Fatalf("海外用户或者云服务器ip被禁止访问网站，请更换ip\n")
-	} else if resp.StatusCode == 401 {
-		gologger.Fatalf("天眼查Cookie有问题或过期，请重新获取\n")
-	} else if resp.StatusCode == 302 {
-		gologger.Fatalf("天眼查免费查询次数已用光，需要加Cookie\n")
-	}
-	resbody, _ := ioutil.ReadAll(resp.Body)
+	data := strings.NewReader("{\"gid\":\""+options.CompanyID+"\",\"pageSize\":200,\"pageNum\":1}")
+	resp := GetPage("https://capi.tianyancha.com/cloud-company-background/company/investListV2",
+		"POST", data, options)
 	var invests InvestList
-	if err := json.Unmarshal(resbody, &invests); err != nil{
+	if err := json.Unmarshal(resp.Body, &invests); err != nil{
 		gologger.Fatalf("查询失败：",err.Error(),"\n")
 	}
 	var subsidiaries []Subsidiary
@@ -151,8 +130,12 @@ func ListInvest(options *Options) []Subsidiary {
 }
 
 func GetCompanyID(options *Options) {
-	CompanyName := url.QueryEscape(options.CompanyName)
-	resp := GetPage("https://sp0.tianyancha.com/search/suggestV2.json?key="+CompanyName, options)
-	re := regexp.MustCompile(`,"id":(\d*),"comName":`)
+	data := strings.NewReader("{\"keyword\":\""+options.CompanyName+"\"}")
+	resp := GetPage("https://capi.tianyancha.com/cloud-tempest/search/suggest/v3",
+		"POST", data, options)
+	re := regexp.MustCompile(`"id":(\d*),"graphId":`)
+	if len(re.FindSubmatch(resp.Body))==0 {
+		gologger.Fatalf("公司名称有误，请更换公司名称\n")
+	}
 	options.CompanyID = string(re.FindSubmatch(resp.Body)[1])
 }
